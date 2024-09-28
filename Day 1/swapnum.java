3586f51eb63578cb4aa2fc7d20d85d1d3a90d6e4
Bharath <bharaths.a2031@gmail.com>
2024-09-28 21:10:22 +0530

    Add files via upload

---
+++ Day 1/swapnum.java
@@ -0,0 +1,14 @@
+public class swapnum
+{
+public static void main(String[] args)
+{
+int a=1;
+int b=5;
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println("After swapping two numbers:");
+System.out.println("a="+a); 
+System.out.println("b="+b);
+}
+}
